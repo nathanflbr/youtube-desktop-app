33f154a9d5f75dfb893d8e6e16b52e15d3f299f3
nathanflbr <71287773+nathanflbr@users.noreply.github.com>
2023-08-23 10:17:46 -0300

    Adds media control bar in OS ✨

--- main.js
+++ main.js
@@ -6,9 +6,35 @@ const createWindow = () => {
     width: 800,
     height: 600,
     icon: __dirname + "/assets/icons/icon.png",
-    webPreferences: {},
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
   });
   mainWindow.loadURL("https://music.youtube.com/");
+
+  const addedBtn = mainWindow.setThumbarButtons([
+    {
+      tooltip: "Previous Track",
+      icon: path.join(__dirname, "assets/icons/skip_previous.png"),
+      click: () => {
+        mainWindow.webContents.send("media-previous-track");
+      },
+    },
+    {
+      tooltip: "Play/Pause",
+      icon: path.join(__dirname, "assets/icons/play_arrow.png"),
+      click() {
+        mainWindow.webContents.send("player-play-toggle");
+      },
+    },
+    {
+      tooltip: "Next Track",
+      icon: path.join(__dirname, "assets/icons/skip_next.png"),
+      click: () => {
+        mainWindow.webContents.send("media-next-track");
+      },
+    },
+  ]);
 };
 
 app.whenReady().then(() => {
--- preload.js
+++ preload.js
@@ -2,6 +2,36 @@
 
 // All the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
+const ipcRenderer = require("electron").ipcRenderer;
+
+ipcRenderer.on("player-play-toggle", () => {
+  const videoElement = document.querySelector("video");
+
+  if (videoElement) {
+    if (videoElement.paused) {
+      videoElement.play();
+    } else {
+      videoElement.pause();
+    }
+  }
+});
+
+ipcRenderer.on("media-previous-track", () => {
+  const previousButton = document.querySelector(".previous-button");
+
+  if (previousButton) {
+    previousButton.click();
+  }
+});
+
+ipcRenderer.on("media-next-track", () => {
+  const nextButton = document.querySelector(".next-button");
+
+  if (nextButton) {
+    nextButton.click();
+  }
+});
+
 window.addEventListener("DOMContentLoaded", () => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector);
